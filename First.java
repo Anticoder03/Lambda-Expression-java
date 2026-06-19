6402d6b8d0502c621e6832f275c26ead9881d308
Anticoder03 <ap5381545@gmail.com>
2026-06-19 20:00:56 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,23 @@
+package LamdaExpressionsDemo;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	/*
+	(length, width) -> {
+        System.out.println("Area is " + length * width);
+    };
+    radius-> System.out.println("Area is " + radius*radius*Math.PI );
+    (num)->{
+    fact=1
+    for(int i=1,=num;i++){
+    fact*=1;
+    }
+    return fact
+    }
+  
+    */
+}
